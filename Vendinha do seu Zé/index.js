9509608a99be948c84cab0253108758cb80ecdc3
Samara Barros <136827899+SohyeH@users.noreply.github.com>
2023-10-04 08:38:49 -0300

    helio project

---
+++ Vendinha do seu Zé/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+const Produto = require('./produtos/Produto')
+const ProdutosController = require ("./produtos/ProdutoController");
+
+app.use("/, pro")
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o Banco de Dados");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//EJS como view engine
+app.set('view engine', 'ejs');
+
+//definindo a pasta de arquivos estaticos
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
